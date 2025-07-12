2f37270aaf181de8e2e94b3fd89b729bce98c731
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-12 09:15:52 -0300

    Update BlocoAnonimo.sql

--- BlocoAnonimo/BlocoAnonimo.sql
+++ BlocoAnonimo/BlocoAnonimo.sql
@@ -1,1 +1,11 @@
 --Bloco anonimo
+SET SERVEROUTPUT ON
+DECLARE
+    vnumero_1 NUMBER(11,2) := 500;
+    vnumero_2 NUMBER(11,2) := 400;
+    vnumero_media NUMBER(11,2);
+BEGIN
+    vnumero_media := (vnumero_1 + vnumero_2) / 2;
+    DBMS_OUTPUT.PUT_LINE('A media e ' || vnumero_media);
+END;
+    
