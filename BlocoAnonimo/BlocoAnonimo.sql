deca64d37b0d86e5ad4ab67aa8c2d893da735d9b
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-12 09:51:21 -0300

    Update BlocoAnonimo.sql

--- BlocoAnonimo/BlocoAnonimo.sql
+++ BlocoAnonimo/BlocoAnonimo.sql
@@ -1,7 +1,7 @@
 --Bloco anonimo
 SET SERVEROUTPUT ON
 DECLARE
-    vnumero_1 NUMBER(11,2) := 500;
+    vnumero_1 NUMBER(11,2) := 553;
     vnumero_2 NUMBER(11,2) := 400;
     vnumero_media NUMBER(11,2);
 BEGIN
