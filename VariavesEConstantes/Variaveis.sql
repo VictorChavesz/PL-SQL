0c8ffbfd3204c205aea40f1b26352b289102e5f3
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-12 09:50:23 -0300

    Update Variaveis.sql

--- VariavesEConstantes/Variaveis.sql
+++ VariavesEConstantes/Variaveis.sql
@@ -4,7 +4,7 @@ SET SERVEROUTPUT ON
 DECLARE
     vNumero NUMBER(11,2):= 100;
 BEGIN
-    DBMS_OUTPUT.PUT_LINE(vNumero);
+    DBMS_OUTPUT.PUT_LINE('Valor = ' || vNumero);
 END;
 
 --Tipo CHAR e VARCHAR2
@@ -14,6 +14,17 @@ DECLARE
     vEstado CHAR(2) := 'GO';
     vCidade VARCHAR2(30) := 'Rio Verde';
 BEGIN
-    DBMS_OUTPUT.PUT_LINE(vEstado);
-    DBMS_OUTPUT.PUT_LINE(vCidade);
+    DBMS_OUTPUT.PUT_LINE('Estado: ' || vEstado);
+    DBMS_OUTPUT.PUT_LINE('Cidade: ' || vCidade);
+END;
+
+--Tipo DATA
+
+SET SERVEROUTPUT ON
+DECLARE 
+    vData1 DATE := '21/11/2004';
+    vData2 DATE := '21/11/25';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(vData1);
+    DBMS_OUTPUT.PUT_LINE(vData2);
 END;
