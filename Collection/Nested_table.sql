d79f3940449d3ec2d3eb7fdaef6c248731249574
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-13 15:21:01 -0300

    Create Nested_table.sql

---
+++ Collection/Nested_table.sql
@@ -0,0 +1,11 @@
+SET SERVEROUTPUT ON
+DECLARE
+    type number_table_type IS TABLE OF NUMBER(3);
+    number_table number_table_type := number_table_type();
+BEGIN
+    FOR i IN 1..10 LOOP
+        number_table.extend;
+        number_table(i) := i;
+        DBMS_OUTPUT.PUT_LINE(number_table(i));
+    END LOOP;
+END;
