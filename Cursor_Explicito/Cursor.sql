6ba562404f737414ad98157c5a23349db131f6ce
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-13 18:19:34 -0300

    Update Cursor.sql

--- Cursor_Explicito/Cursor.sql
+++ Cursor_Explicito/Cursor.sql
@@ -1,3 +1,5 @@
+-- PERCORRENDO COM LOOP BASICO
+
 SET SERVEROUTPUT ON
 DECLARE
     CURSOR cEMPLOYEES IS
@@ -27,4 +29,34 @@ BEGIN
                             employees_record.job_id          || ' - ' ||
                             TO_CHAR(employees_record.salary)
                          ); 
+    CLOSE cEMPLOYEES;
+END;
+
+--PERCORRENDO COM WHILE LOOP
+
+SET SERVEROUTPUT ON
+DECLARE
+    CURSOR cEMPLOYEES IS
+    SELECT * FROM EMPLOYEES;
+    employees_record cEMPLOYEES%ROWTYPE;
+BEGIN
+    OPEN cEMPLOYEES;
+    
+    FETCH cEMPLOYEES
+        INTO employees_record;
+    
+    WHILE cEMPLOYEES%FOUND LOOP
+    
+        DBMS_OUTPUT.PUT_LINE(employees_record.employee_id    || ' - ' || 
+                            employees_record.first_name      || ' - ' || 
+                            employees_record.last_name       || ' - ' ||
+                            employees_record.phone_number    || ' - ' ||
+                            employees_record.job_id          || ' - ' ||
+                            TO_CHAR(employees_record.salary)
+                         ); 
+        FETCH cEMPLOYEES
+        INTO employees_record;
+    END LOOP;
+    
+    CLOSE cEMPLOYEES;
 END;
