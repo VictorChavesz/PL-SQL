9a4aa3c326987e9fe5191ff52e214c4ae0404eaa
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-13 11:17:11 -0300

    Create TESTE.sql

---
+++ Estruturas_de_controle/TESTE.sql
@@ -0,0 +1,19 @@
+SET SERVEROUTPUT ON
+ACCEPT limite PROMPT 'Digite o valor do limite: '
+DECLARE
+    vResultado NUMBER(3);
+    vLimite NUMBER(11) := &limite;
+BEGIN
+DBMS_OUTPUT.PUT_LINE('Loop da tabuada do 1 ate o ' || vLimite);
+<<LOOP1>>
+    FOR i IN 1..vLimite LOOP
+        DBMS_OUTPUT.PUT_LINE('Tabuada do ' || i);
+        <<LOOP2>>
+            FOR j IN 1..10 LOOP
+                vResultado := i * j;
+                DBMS_OUTPUT.PUT_LINE(i || ' X ' || j || ' = ' || vResultado);
+            END LOOP;
+            
+            EXIT LOOP1 WHEN i = vLimite;
+    END LOOP;
+END;
