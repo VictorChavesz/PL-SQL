f875abd6146b9380e04f0029cfebae4ce04b0d47
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-12 21:28:26 -0300

    Update CASE.sql

--- Estruturas_de_controle/CASE.sql
+++ Estruturas_de_controle/CASE.sql
@@ -1,3 +1,5 @@
+--Case simples
+
 SET SERVEROUTPUT ON
 ACCEPT pdepartment_id PROMPT 'Digite o Id do departamento: '
 DECLARE
@@ -5,20 +7,31 @@ DECLARE
     vDeparatamentoId            employees.DEPARTMENT_ID%type := &pdepartment_id;
 BEGIN
     CASE vDeparatamentoId
-    WHEN 50
-    THEN 
-        vPercentualAumento := 20;
-    WHEN 30
-    THEN
-        vPercentualAumento := 17.5;
-    WHEN 10
-    THEN
-        vPercentualAumento := 15;
-    WHEN 60
-    THEN
-        vPercentualAumento := 12.5;
-    ELSE
-        vPercentualAumento := 10;
+    WHEN 50 THEN vPercentualAumento := 20;
+    WHEN 30 THEN vPercentualAumento := 17.5;
+    WHEN 10 THEN vPercentualAumento := 15;
+    WHEN 60 THEN vPercentualAumento := 12.5;
+    ELSE vPercentualAumento := 10;
+    END CASE;
+    
+    DBMS_OUTPUT.PUT_LINE('Id  do departamento: ' || vDeparatamentoId);
+    DBMS_OUTPUT.PUT_LINE('Percentual de aumento : ' || vPercentualAumento || '%');
+END;
+
+--Searched case
+
+SET SERVEROUTPUT ON
+ACCEPT pdepartment_id PROMPT 'Digite o Id do departamento: '
+DECLARE
+    vPercentualAumento          NUMBER(3);
+    vDeparatamentoId            employees.DEPARTMENT_ID%type := &pdepartment_id;
+BEGIN
+    CASE 
+    WHEN vDeparatamentoId = 50 THEN vPercentualAumento := 20;
+    WHEN vDeparatamentoId = 30 THEN vPercentualAumento := 17.5;
+    WHEN vDeparatamentoId = 10 THEN vPercentualAumento := 15;
+    WHEN vDeparatamentoId = 60 THEN vPercentualAumento := 12.5;
+    ELSE vPercentualAumento := 10;
     END CASE;
     
     DBMS_OUTPUT.PUT_LINE('Id  do departamento: ' || vDeparatamentoId);
