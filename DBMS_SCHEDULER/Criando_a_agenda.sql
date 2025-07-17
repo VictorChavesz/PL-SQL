c28b61f40230dcd06c34d9ef8ab89ecbeea32657
VictorChavesz <paulovictorsilva122@gmail.com>
2025-07-17 11:30:08 -0300

    alterações

--- DBMS_SCHEDULER/Criando_a_agenda.sql
+++ DBMS_SCHEDULER/Criando_a_agenda.sql
@@ -1,9 +1,9 @@
 BEGIN
     DBMS_SCHEDULER.CREATE_SCHEDULE(
-        schedule_name => 'SCH_A_CADA_DIA',
+        schedule_name => 'SCH_A_CADA_MINUTO',
         start_date => (SYSTIMESTAMP),
         -- ou TO_TIMESTAMP_TZ('2025-07-17 10.17.000000000 AMERICA/SAO PAULO','YYYY-MM-DD HH24:MI:SS.FF TZR'),
-        repeat_interval =>  'FREQ = DAILY',
+        repeat_interval =>  'FREQ = MINUTELY',
         end_date => SYSTIMESTAMP + INTERVAL '5' DAY,
         comments => '1 vez ao dia'
     );
--- DBMS_SCHEDULER/Criando_job.sql
+++ DBMS_SCHEDULER/Criando_job.sql
@@ -1,11 +1,23 @@
 BEGIN
     DBMS_SCHEDULER.CREATE_JOB(
         job_name        => 'JOB_EXECUTA_INSERE_AGENDA',
-        program_name    => 'HR.INSERE_DATA_AGENDA1',
-        schedule_name   => 'SCH_A_CADA_DIA'
-    );
-    
-    DBMS_SCHEDULER.ENABLE(
-        name => 'JOB_EXECUTA_INSERE_AGENDA'
+        program_name    => 'HR.INSERE_DATA_AGENDA3',
+        schedule_name   => 'SCH_A_CADA_MINUTO',
+        enable          => TRUE
     );
 END;
+
+BEGIN
+    DBMS_SCHEDULER.DISABLE(
+    name => 'JOB_EXECUTA_INSERE_AGENDA'
+    );
+END;
+
+SELECT job_name, status, error#, additional_info, actual_start_date
+FROM USER_SCHEDULER_JOB_RUN_DETAILS
+WHERE job_name = 'JOB_EXECUTA_INSERE_AGENDA'
+ORDER BY actual_start_date DESC;
+
+SELECT * FROM USER_SCHEDULER_JOBS;
+
+SELECT * FROM AGENDA1;
